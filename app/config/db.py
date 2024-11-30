c59206b4d283939eada1477f2b5dda22b76eb05e
Juzo(-.-) <alexs22221@gmail.com>
2024-12-01 02:01:53 +0300

    aaa

--- app/config/db.py
+++ app/config/db.py
@@ -4,15 +4,14 @@ import os
 
 def get_db_url():
     # if os.environ.get('DEBUG', False):
-    DB_USER =  os.environ.get('USER', 'root')
-    DB_PASSWORD =  os.environ.get('PASSWORD', '')
-    DB_HOST =  os.environ.get('HOST', 'localhost')
-    DB_PORT = os.environ.get('PORT', '3306')
-    DB_NAME =  os.environ.get('DB', 'hackchange')
-    # else:
-    #     DB_USER = "root"
-    #     DB_PASSWORD = "eY7m76S_rP"
-    #     DB_HOST = "localhost"
-    #     DB_NAME = "hackchange"
-    #     DB_PORT = "3306"
+    # DB_USER =  os.environ.get('USER', 'root')
+    # DB_PASSWORD =  os.environ.get('PASSWORD', 'eY7m76S_rP')
+    # DB_HOST =  os.environ.get('HOST', 'localhost')
+    # DB_PORT = os.environ.get('PORT', '3306')
+    # DB_NAME =  os.environ.get('DB', 'hackchange')
+    DB_USER = "root"
+    DB_PASSWORD = ""
+    DB_HOST = "localhost"
+    DB_NAME = "hackchange"
+    DB_PORT = "3306"
     return f"mysql+asyncmy://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
